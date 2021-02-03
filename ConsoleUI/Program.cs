82588535dc6cea12e10a97315ef71838476f5ce9
PC <PC@DESKTOP-GVFF6BH>
2021-02-04 00:04:08 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,22 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var product in productManager.GetByUnitPrice(5, 100)) 
+            {
+                Console.WriteLine(product.ProductName);
+
+            }
+        }
+    }
+}
